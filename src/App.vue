af419ea9bb8a587ed8019f7e5385ffb7faf41877
Enhui Zhu <enhuizhu@Enhuis-MacBook-Pro.local>
2020-03-07 23:16:13 +0000

    create the basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,132 @@
 <template>
   <div id="app">
-    <h1>here is fraction circle</h1>
-    <FractionCircle></FractionCircle>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="result-container">
+      <ul>
+        <li>
+          <label>Wrong:</label>
+          {{this.wrongAnswers}}
+        </li>
+        <li>
+          <label>Correct:</label>
+          {{this.correctAnswers}}
+        </li>
+        <li>
+          <label>Percentage:</label>
+          {{this.getPercentage()}}
+        </li>
+      </ul>
+    </div>
+    <div class="fraction-container">
+      <FractionCircle v-bind:denominator="firstFraction.denominator" v-bind:numerator="firstFraction.numerator"></FractionCircle>
+      <span class="operator">+</span>
+      <FractionCircle v-bind:denominator="secondFraction.denominator" v-bind:numerator="secondFraction.numerator"></FractionCircle>
+      <!-- <span class="operator">=</span> -->
+    </div>
 
+    <div class="fraction-container">
+      <div class="text">{{`${firstFraction.numerator} / ${firstFraction.denominator}`}}</div>
+      <span class="operator">+</span>
+      <div class="text">{{`${secondFraction.numerator} / ${secondFraction.denominator}`}}</div>
+      <!-- <span class="operator">=</span> -->
+    </div>
+
+    <div class="answer-container">
+      <form v-on:submit="handleSubmit">
+        <div class="form-control-group center">
+          <label>Answer:</label>
+          <input type="text" v-model="answer">
+        </div>
+        <div class="form-control-group center" style="margin-top: 15px">
+          <input type="submit" value="next" class="btn">
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import FractionCircle from './components/FractionCircle';
 
 export default {
   name: 'App',
+  
+  data: () => {
+    return {
+      answer: '',
+      percentage: '0%',
+      wrongAnswers: 0,
+      correctAnswers: 0,
+      firstFraction: {},
+      secondFraction: {},
+    }
+  },
+  
+  methods: {
+    handleSubmit(event) {
+      event.preventDefault();
+
+      let firstFraction = this.firstFraction.numerator / this.firstFraction.denominator;
+      let secondFraction = this.secondFraction.numerator / this.secondFraction.denominator;
+
+      if (firstFraction + secondFraction === eval(this.answer.replace(/[^0-9/]/g), '')) {
+        this.correctAnswers++;
+      } else {  
+        this.wrongAnswers++;
+      }
+
+      this.generateNewFractionNumbers();
+    },
+
+    generateFractionObjcet() {
+      // generate value from 1 - 10
+      let denominator = Math.round(Math.random() * 9) + 1;
+      // generate value from 0 - 10
+      let numerator = Math.round(Math.random() * denominator);
+
+      return {
+        denominator,
+        numerator
+      }
+    },
+
+    getPercentage() {
+      if (this.correctAnswers + this.wrongAnswers === 0) {
+        return '0.00%';
+      }
+
+      return `${(this.correctAnswers / (this.wrongAnswers + this.correctAnswers) * 100).toFixed(2)} %`;
+    },
+
+    generateNewFractionNumbers() {
+      this.firstFraction = this.generateFractionObjcet();
+      this.secondFraction = this.generateFractionObjcet();
+    }
+  },
+
   components: {
-    HelloWorld,
     FractionCircle,
+  },
+
+  created: function() {
+    this.generateNewFractionNumbers();
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+  .fraction-container {
+    margin-top: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .text {
+      width: 100px;
+      text-align: center;
+    }
+
+    .operator {
+      margin: 0 10px;
+    }
+  }
 </style>
--- src/components/FractionCircle.vue
+++ src/components/FractionCircle.vue
@@ -7,59 +7,109 @@
 <script>
 export default {
   name: 'FractionCircle',
+  
   data() {
     return {
       width: 200,
       height: 200,
     }
   },
+  
   props: {
     denominator: Number,
     numerator: Number,
   },
 
+  watch:{
+    'denominator'() {
+      console.log('denominator', this.denominator);
+      this.draw();
+    },
+    'numerator'() {
+      console.log('numerator', this.numerator);
+      this.draw();
+    },
+  },
+
   methods: {
-    setupCanvasEnv: function() {
+    setupCanvasEnv() {
       this.c = this.$refs['circle'];
       this.ctx = this.c.getContext('2d');
+    },
+    
+    draw() {
+      this.clear();
       this.drawBackgroud();
+      this.drawSections();
     },
 
     drawBackgroud() {
-      const centerX = this.width / 2;
-      const centerY = this.height / 2;
-      const readius = this.width / 2 - 1;
-      this.ctx.strokeStyle = "#FF0000";
-      this.ctx.strokeRect(20, 20, 150, 100);
+      this.ctx.save();
+      this.centerX = this.width / 2;
+      this.centerY = this.height / 2;
+      this.readius = this.width / 2 - 1;
+      this.ctx.strokeStyle = "#000000";
       this.ctx.beginPath();
-      this.ctx.arc(centerX, centerY, readius, 0, Math.PI * 2);
+      this.ctx.arc(this.centerX, this.centerY, this.readius, 0, Math.PI * 2);
       this.ctx.stroke();
+      this.ctx.restore();
+
+      this.drawSections();
+    },
+
+    clear() {
+      try {
+        this.ctx.clearRect(0, 0, this.c.width, this.c.height);
+      } catch(e) {
+        console.log('clear error', e);
+      }
+    },
+
+    drawSections() {
+      this.ctx.save();
+      const totoalAngles = Math.PI * 2 / this.denominator;
+      this.ctx.strokeStyle = "#000000";
+
+      for (let i = 0; i < this.numerator; i++) {
+        const startAngles = i * totoalAngles;
+        const endAngles = startAngles + totoalAngles;
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.centerX, this.centerY);
+        this.ctx.arc(this.centerX, this.centerY, this.readius, startAngles, endAngles);
+        this.ctx.moveTo(this.centerX, this.centerY);
+        this.ctx.closePath();
+        this.ctx.fillStyle='green';
+        this.ctx.fill();
+        this.ctx.stroke();
+      }
+
+      this.ctx.restore();
     }
   },
 
   created: function() {
-
+    console.log('created');
   },
 
   mounted: function() {
+    console.log('mounted');
     this.setupCanvasEnv();
-    console.log('vue', this.$refs['circle']);
-    
+    this.draw();
   },
 
   updated: function() {
-
+    console.log('updated');
   },
 
   destroyed: function() {
-
+    console.log('destroyed');
   },
 }
 </script>
 
 <style scoped>
   .circle {
-    width: 200;
-    height: 200;
+    width: 100px;
+    height: 100px;
   }
 </style>
