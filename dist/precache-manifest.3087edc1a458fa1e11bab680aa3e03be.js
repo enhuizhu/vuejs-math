b89fe2119e06624d085c349749ee0cc815e7814b
Enhui Zhu <enhuizhu@Enhuis-MacBook-Pro.local>
2020-03-12 21:36:26 +0000

    add dist

---
+++ dist/precache-manifest.3087edc1a458fa1e11bab680aa3e03be.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c886d0a73fefaaf5d57",
+    "url": "/css/app.d91c836f.css"
+  },
+  {
+    "revision": "6f306f2173106e46a5dcc3e58502d985",
+    "url": "/img/bg.6f306f21.jpg"
+  },
+  {
+    "revision": "a78cc258832ce4a6eaedfe68602c8af0",
+    "url": "/img/bg2.a78cc258.png"
+  },
+  {
+    "revision": "fd06807f7ae09733f4e1d56a1821e111",
+    "url": "/img/bg3.fd06807f.png"
+  },
+  {
+    "revision": "6bdeee409ee6f1a972ac00ce28148a84",
+    "url": "/img/bg4.6bdeee40.jpg"
+  },
+  {
+    "revision": "2e0f82d8fb58161ce5cc2e7fb99cbfb2",
+    "url": "/img/bg5.2e0f82d8.webp"
+  },
+  {
+    "revision": "976e528a8564476d0d6dd55ad36406b0",
+    "url": "/img/bg6.976e528a.jpeg"
+  },
+  {
+    "revision": "408928e22e9ebb1fb5b451fafa0bc249",
+    "url": "/img/sorry2.408928e2.gif"
+  },
+  {
+    "revision": "0d8fc694b85a9ac6779cc201c3f4c5c6",
+    "url": "/img/welldone.0d8fc694.gif"
+  },
+  {
+    "revision": "6f8f98e3d3c0a0f27b8cb2121d09b560",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c886d0a73fefaaf5d57",
+    "url": "/js/app.5f306ec1.js"
+  },
+  {
+    "revision": "3c20bfea9d2cdb1a121d",
+    "url": "/js/chunk-vendors.6cdb63f5.js"
+  },
+  {
+    "revision": "84e6c4d190079b45a0502c4564eea27f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "324b540b6d92b9f4c87925253ede6987",
+    "url": "/media/sad.324b540b.wav"
+  },
+  {
+    "revision": "baad2ca967e715ae6f4e9207471343f3",
+    "url": "/media/sad.baad2ca9.mp3"
+  },
+  {
+    "revision": "3f7085c4c2650d1bb35d552648652d58",
+    "url": "/media/tick.3f7085c4.mp3"
+  },
+  {
+    "revision": "5d96737c7a4fd290afc158ba552f5032",
+    "url": "/media/tick.5d96737c.wav"
+  },
+  {
+    "revision": "1768de81fa3eef9da9ce4aedc029bc69",
+    "url": "/media/win.1768de81.mp3"
+  },
+  {
+    "revision": "9aefd12553b1666cbe345dd7c1d78fd2",
+    "url": "/media/win.9aefd125.wav"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
